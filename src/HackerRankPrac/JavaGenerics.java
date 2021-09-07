e84e1e06b55f59e141608402cba72b0f96b215d1
leehd660 <86194199+leehd660@users.noreply.github.com>
2021-09-07 22:22:18 +0900

    09.07 Java Generics 문제 해결

--- src/HackerRankPrac/JavaGenerics.java
+++ src/HackerRankPrac/JavaGenerics.java
@@ -1,4 +1,37 @@
 package HackerRankPrac;
 
+import java.lang.reflect.Method;
+
 public class JavaGenerics {
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+
+    }
+
+    static class Printer {
+        //Write your code here
+        public void printArray(Object[] arr){
+
+            for (Object object : arr){
+                System.out.println(object);
+            }
+
+        }
+
+    }
 }
